b6e8f9682932ef9e2441c57eef60cd908087cd0b
sadiqss <sadiq.sau48@gmail.com>
2022-04-24 14:51:39 +0100

    delete update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AddUser from './component/AddUser/AddUser';
 import Home from './component/Home/Home';
+import UpdateUser from './component/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='user/add' element={<AddUser></AddUser>}></Route>
+        <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
       </Routes>
     </div>
   );
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -11,6 +12,18 @@ const Home = () => {
         const proceed = window.confirm('Are you sure to delete?');
         if (proceed) {
             console.log('deleting user with id', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = users.filter(user => user._id !== id);
+                        setUsers(remaining);
+                    }
+                })
         }
 
     }
@@ -22,6 +35,7 @@ const Home = () => {
                     users.map(user => <li
                         key={user._id}
                     >{user.name}::{user.email}
+                        <Link to={`/update/${user._id}`}><button>Update</button></Link>
                         <button onClick={() => handleUserDelete(user._id)}>X</button>
                     </li>)
                 }
